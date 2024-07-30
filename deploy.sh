2688f025aec3356f45c3e10b3b8e496c7dfc8d1c
Brent Michalski <Brent.Michalski@gmail.com>
2024-07-30 16:19:23 -0500

    . e Adding host to known_hosts

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ HOST="iad1-shared-e1-02.dreamhost.com"
 REMOTE_DIR="/perlguy.net"
 LOCAL_DIR="/Users/brent/Development/IgniteWebSites/Ignite/PerlguySite/Build"
 
+# Ensure the known_hosts file exists and add the Dreamhost server to it
+ssh-keyscan $HOST >> ~/.ssh/known_hosts
+
 # Sync files using rsync
 rsync -avz --delete $LOCAL_DIR $USER@$HOST:$REMOTE_DIR
 
