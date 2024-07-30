38b7f94e18431e8f6899627dfcd5515030b06417
Brent Michalski <Brent.Michalski@gmail.com>
2024-07-30 16:36:39 -0500

    . e Fixed, hopefully, the deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 # Define our variables
 USER="perlguy9"
 HOST="iad1-shared-e1-02.dreamhost.com"
-REMOTE_DIR="/perlguy.net"
-LOCAL_DIR="/Users/brent/Development/IgniteWebSites/Ignite/PerlguySite/Build"
+REMOTE_DIR="~/perlguy.net"
+LOCAL_DIR="./Build"
 
 # Ensure the known_hosts file exists and add the Dreamhost server to it
-ssh-keyscan $HOST >> ~/.ssh/known_hosts
+#ssh-keyscan $HOST >> ~/.ssh/known_hosts
 
 # Sync files using rsync
 rsync -avz --delete $LOCAL_DIR $USER@$HOST:$REMOTE_DIR
